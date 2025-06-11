12e9b821419b689471f074d1cc45671f34ea3234
Jeswin A.H <jeswinah@example.com>
2025-06-11 11:24:22 +0530

    Sprint-1-B-calculator

---
+++ Job_simulation/Sprint-II/Sprint-1-B-AI_Mode/script.js
@@ -0,0 +1,32 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const operation = document.querySelector(".operation-btn");
+const results = document.getElementById("result");
+
+const calculate = (operation) => {
+  let result;
+  let n1 = Number(num1.value);
+  let n2 = Number(num2.value);
+  switch (operation) {
+    case "add":
+      result = n1 + n2;
+      break;
+    case "subtract":
+      result = n1 - n2;
+      break;
+    case "multiply":
+      result = n1 * n2;
+      break;
+    case "divide":
+      result = n1 / n2;
+      break;
+  }
+  results.innerHTML = result;
+};
+
+const clearAll = () => {
+  num1.value = null;
+  num2.value = null;
+  results.innerHTML = "Result will appear here";
+};
+
