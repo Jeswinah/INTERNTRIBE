4c1e7cd0f49dfd1a53bbc5cd040db898a8cd7696
Jeswin A.H <jeswinah@example.com>
2025-06-11 10:05:48 +0530

    Commands updated

--- Job_simulation/Sprint-II/Sprint-1-A/script.js
+++ Job_simulation/Sprint-II/Sprint-1-A/script.js
@@ -7,7 +7,9 @@ const timehandler = () => {
   const hrs = date.getHours();
   const mins = date.getMinutes();
   const secs = date.getSeconds();
+  // if the time is greater than 12 then it update PM otherwise AM
   if (hrs > 12) {
+    // The time is in the form of 24hours.So hrs%12 it convert into a 12 hours format
     hr.innerHTML = hrs % 12;
     am.innerHTML = "Pm";
     if (hrs <= 21) {
@@ -22,6 +24,7 @@ const timehandler = () => {
   const m = mins < 10 ? (min.innerHTML = "0" + mins) : (min.innerHTML = mins);
 };
 
+// Every 1 sec it calls the timehandler function
 setInterval(() => {
   timehandler();
 }, 1000);
